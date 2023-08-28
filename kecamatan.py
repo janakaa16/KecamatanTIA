23188d477885f36600055ada5f987aa1e2bb7e27
janakaa16 <113839927+janakaa16@users.noreply.github.com>
2023-08-28 08:54:41 +0700

    Update kecamatan.py

--- kecamatan.py
+++ kecamatan.py
@@ -1,90 +1,58 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Load data
+@st.cache
+def load_data():
+    return pd.read_excel('KecamatanAgustus2023.xlsx')
+
+@st.cache
+def load_data2():
+    return pd.read_excel('KotaAgustus2023.xlsx')
+
+df = load_data()
+df2 = load_data2()
+
+# Set Streamlit options
 st.set_option('deprecation.showPyplotGlobalUse', False)
-df = pd.read_excel('KecamatanAgustus2023.xlsx')
 
-st.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=True)
+# Set background color and title
+st.markdown('<style>body{background-color: Blue;}</style>', unsafe_allow_html=True)
 st.title("Kecamatan Terbaik Berdasarkan Kota/Povinsi")
 
-# Create dropdown list with 'All' option
+# Dropdown for City selection
 city_options = ['All'] + list(df["Kota"].unique())
 selected_city = st.selectbox("Choose a city", city_options)
 
-# Display data based on selected city
+# Dropdown for Region selection
+selected_region = st.selectbox("Choose a region", df2["Provinsi"].unique())
+
+# Filter data based on selections
 if selected_city == 'All':
-    selected_rows = df  # Show all rows
+    selected_rows_city = df
 else:
-    selected_rows = df[df["Kota"] == selected_city]  # Filter by selected city
+    selected_rows_city = df[df["Kota"] == selected_city]
 
-# Create count plot
-plt.figure(figsize=(20, 10))
-ax = sns.countplot(data=selected_rows, x=' kecamatan', order=selected_rows[' kecamatan'].value_counts().head(10).index)
-plt.xticks(rotation=20, size=20)
-plt.title(f"Count Plot for Kecamatan in {selected_city if selected_city != 'All' else 'All Cities'}")
-plt.tight_layout()
+selected_rows_region = df2[df2["Provinsi"] == selected_region]
 
-# Annotate the bars with count values
-for p in ax.patches:
-    ax.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
-                ha='center', va='center', fontsize=12, color='gray', xytext=(0, 5),
-                textcoords='offset points')
+# Function to create and display count plot
+def display_count_plot(data, x_column, title):
+    plt.figure(figsize=(20, 10))
+    ax = sns.countplot(data=data, x=x_column, order=data[x_column].value_counts().head(10).index)
+    plt.xticks(rotation=20, size=20)
+    plt.title(title)
+    plt.tight_layout()
 
-# Display the plot using st.pyplot
-st.pyplot()
+    # Annotate the bars with count values
+    for p in ax.patches:
+        ax.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                    ha='center', va='center', fontsize=12, color='gray', xytext=(0, 5),
+                    textcoords='offset points')
 
-selected_region = st.selectbox("Choose a region", df["Provinsi"].unique())
+    st.pyplot()
 
-# Display data based on selected city
-selected_rows2 = df[df["Provinsi"] == selected_region]
-
-plt.figure(figsize=(20, 10))
-ax = sns.countplot(data=selected_rows2, x='Kota', order=selected_rows2['Kota'].value_counts().head(10).index)
-plt.xticks(rotation=20, size=20)
-plt.title(f"Count Plot for Kota in {selected_region}")
-plt.tight_layout()
-
-# Annotate the bars with count values
-for p in ax.patches:
-    ax.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
-                ha='center', va='center', fontsize=12, color='gray', xytext=(0, 5),
-                textcoords='offset points')
-
-st.pyplot()
-
-# New Jabodetabek vs Luar
-selected_loc = st.selectbox("Choose a location", df["Lokasi"].unique())
-
-# Display data based on selected city
-selected_rows3 = df[df["Lokasi"] == selected_loc]
-
-plt.figure(figsize=(20, 10))
-ax = sns.countplot(data=selected_rows3, x='Kota', order=selected_rows3['Kota'].value_counts().head(10).index)
-plt.xticks(rotation=20, size=20)
-plt.title(f"Count Plot for Kota in {selected_loc}")
-plt.tight_layout()
-
-# Annotate the bars with count values
-for p in ax.patches:
-    ax.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
-                ha='center', va='center', fontsize=12, color='gray', xytext=(0, 5),
-                textcoords='offset points')
-
-st.pyplot()
-
-plt.figure(figsize=(20, 10))
-ax = sns.countplot(data=selected_rows3, x=' kecamatan', order=selected_rows3[' kecamatan'].value_counts().head(10).index)
-plt.xticks(rotation=20, size=20)
-plt.title(f"Count Plot for kecamatan in {selected_loc}")
-plt.tight_layout()
-
-# Annotate the bars with count values
-for p in ax.patches:
-    ax.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
-                ha='center', va='center', fontsize=12, color='gray', xytext=(0, 5),
-                textcoords='offset points')
-
-st.pyplot()
+# Display count plots
+display_count_plot(selected_rows_city, ' kecamatan', f"Count Plot for Kecamatan in {selected_city if selected_city != 'All' else 'All Cities'}")
+display_count_plot(selected_rows_region, 'Kota', f"Count Plot for Kota in {selected_region}")
